18fd6d59c2742e003f8f85be87659d01f75c3c38
Hyeonjong <nianelo4@gmail.com>
2024-03-10 16:12:35 +0900

    Implement prettier v3 formatting

--- src/components/output-area/index.tsx
+++ src/components/output-area/index.tsx
@@ -2,11 +2,12 @@ import { useOutputArea } from './hooks';
 
 export function OutputArea({ version }: { version: 2 | 3 }) {
   const { formattingResult } = useOutputArea(version);
+  const prettierVersion = version === 2 ? '2.8.4' : '3.0.3';
 
   return (
     <label className="form-control h-full">
       <div className="label">
-        <span className="label-text">Output (v{version})</span>
+        <span className="label-text">Output (v{prettierVersion})</span>
       </div>
       <textarea
         readOnly
--- src/components/output-area/libs.ts
+++ src/components/output-area/libs.ts
@@ -2,6 +2,11 @@ import { format as format2 } from 'prettier2';
 import babelPlugin2 from 'prettier2/parser-babel';
 import htmlPlugin2 from 'prettier2/parser-html';
 import typescriptPlugin2 from 'prettier2/parser-typescript';
+import babelPlugin3 from 'prettier3/plugins/babel';
+import estreePlugin3 from 'prettier3/plugins/estree';
+import htmlPlugin3 from 'prettier3/plugins/html';
+import typescriptPlugin3 from 'prettier3/plugins/typescript';
+import { format as format3 } from 'prettier3/standalone';
 
 export function v2Format(
   ...[source, options]: Parameters<typeof format2>
@@ -12,6 +17,11 @@ export function v2Format(
   });
 }
 
-export async function v3Format(source: string, options?: any): Promise<string> {
-  return source;
+export async function v3Format(
+  ...[source, options]: Parameters<typeof format3>
+): Promise<string> {
+  return format3(source, {
+    ...options,
+    plugins: [babelPlugin3, estreePlugin3, htmlPlugin3, typescriptPlugin3],
+  });
 }
