12d0c910829e508841a9334370a0c61fa0d66a66
Hyeonjong <nianelo4@gmail.com>
2024-04-10 23:34:14 +0900

    Revert "Caching with react-query on client"
    This reverts commit 6ea3bab07b353aea95dbeddc9ba6eddd6a7729b0.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,17 @@
 import '@/styles/globals.css';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
 import { classNames } from '@/adaptors';
 import { pretendard } from '@/fonts';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <main className={classNames(pretendard.variable, 'font-sans')}>
-          <Component {...pageProps} />
-        </main>
-      </QueryClientProvider>
+      <main className={classNames(pretendard.variable, 'font-sans')}>
+        <Component {...pageProps} />
+      </main>
     </RecoilRoot>
   );
 }
--- src/shared-kernel/utils.ts
+++ src/shared-kernel/utils.ts
@@ -1,4 +1,3 @@
-import { createHash } from 'crypto';
 import type { ZodTypeAny, infer as ZodInfer } from 'zod';
 
 export function isTypeof<T extends ZodTypeAny>(
@@ -6,8 +5,4 @@ export function isTypeof<T extends ZodTypeAny>(
   expectedSchema: T,
 ): arg is ZodInfer<T> {
   return expectedSchema.safeParse(arg).success;
-}
-
-export function sha1(input: string): string {
-  return createHash('sha1').update(input).digest('hex');
 }
